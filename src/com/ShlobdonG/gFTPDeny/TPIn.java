312e7a3663ec39001f7e45c94d007ffcb64d191f
Britton <brittongg@gmail.com>
2016-12-30 16:54:25 -0600

    Rewrite of original code to more efficient, easier to read layout.

--- src/com/ShlobdonG/gFTPDeny/TPIn.java
+++ src/com/ShlobdonG/gFTPDeny/TPIn.java
@@ -1,6 +1,5 @@
 package com.ShlobdonG.gFTPDeny;
 
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,122 +15,66 @@ import com.massivecraft.massivecore.ps.PS;
 
 public class TPIn implements Listener {
 
-	private gFTPDenyMain plugin;
-	  public TPIn(gFTPDenyMain inst)
-	  {
-	    this.plugin = inst;
-	  }
-	
-	  /* Enemy */
-	    // This one is to stop players TP INTO enemy territory
-	  @EventHandler
-	    private void onPlayerEnemyTPIn(PlayerTeleportEvent eventei) {
-	        Player player = eventei.getPlayer();
-	        MPlayer mplayer = MPlayer.get(player);
-	        Faction faction;
-	        // Get the current relation of location claim player is headed
-	        faction = BoardColl.get().getFactionAt(PS.valueOf(eventei.getTo()));
-	        Faction factionout;
-	        factionout = BoardColl.get().getFactionAt(PS.valueOf(eventei.getFrom()));
-	        if (mplayer.getFaction().getRelationTo(factionout) == Rel.ENEMY) {
-	        	return;
-	        } else {
-	        // Get the config option for this event
-	        if (this.plugin.config.getBoolean("enemyDenyTPINTO")) {
-	        // Check to see if the faction claim player is standing on is enemy, then cancel the TP INTO
-	        if (mplayer.getFaction().getRelationTo(faction) == Rel.ENEMY) {
-	            eventei.setCancelled(true);
-	            player.sendMessage("§c§l[MCE] §r§cYou cannot tp in to enemy land.");
-	        } 
-	        } else {
-	        	return;
-	        }
-	        }
-	      
-	    }
-	  
-	  @EventHandler
-	    /* Neutral */
-	     // This is so players cant tp into Neutral territory
-	    private void onPlayerNeutralTPIn(PlayerTeleportEvent eventni) {
-	        // The Player
-	        Player playerni = eventni.getPlayer();
-	        MPlayer mplayerni = MPlayer.get(playerni);
-	        // Making 'faction' usable later in code and so it can be defined.
-	        Faction factionni; 
-	        // Get the current relation of location claim player is headed
-	        factionni = BoardColl.get().getFactionAt(PS.valueOf(eventni.getTo()));
-	        // This gets wild, safe, and war zones defined so we can use it later in the checks!
-	        Faction wild = FactionColl.get().getNone();
-	        String wildId = wild.getId();
-	        Faction war = FactionColl.get().getWarzone();
-	        String warId = war.getId();
-	        Faction safe = FactionColl.get().getSafezone();
-	        String safeId = safe.getId();
-	        
-	        // Defines another version of 'faction' to stop message sending when not needed
-	        Faction factionoutni;
-	        factionoutni = BoardColl.get().getFactionAt(PS.valueOf(eventni.getFrom()));
-	        if (mplayerni.getFaction().getRelationTo(factionoutni) == Rel.NEUTRAL) {
-	        	return;
-	        } else {
-	            // Checks if the faction is wild, safe or war zones, if so it will continue their teleport! Else, it will stop their teleport!
-	            if (factionni.getId() == warId || factionni.getId() == wildId || factionni.getId() == safeId) {
-	                return;
-	            } else {
-		        	if (this.plugin.config.getBoolean("neutralDenyTPINTO")) {
-	        // Checks if the player is neutral to the faction claims they are in
-	        if (mplayerni.getFaction().getRelationTo(factionni) == Rel.NEUTRAL) {
-	            //checks if the faction is wild, safe or war zones, if so it will continue their teleport! Else, it will stop their teleport!
-	                eventni.setCancelled(true);
-	                playerni.sendMessage("§c§l[MCE] §r§cYou cannot tp into neutral land.");
-            }
-        } else {
-        	return;
-        }
-        	}
-	        }
-	  }
+    // Allowing code in the main class be called.
+    private gFTPDenyMain plugin;
+    public TPIn(gFTPDenyMain inst) {
+        this.plugin = inst;
+    }
 
-	    
-	  @EventHandler
-	    /* Truce */
-	     // This is so players cant tp into Truce territory
-	    private void onPlayerTruceTPIn(PlayerTeleportEvent eventti) {
-	        // The Player
-	        Player player = eventti.getPlayer();
-	        MPlayer mplayer = MPlayer.get(player);
-	        // Making 'faction' usable later in code and so it can be defined.
-	        Faction faction; 
-	        // Get the current relation of location claim player is headed
-	        faction = BoardColl.get().getFactionAt(PS.valueOf(eventti.getTo()));
-	        // Making Wild, Safe, & War zones usable later in the code.
-	        Faction wildTI = FactionColl.get().getNone();
-	        String wildIdTI = wildTI.getId();
-	        Faction warTI = FactionColl.get().getWarzone();
-	        String warIdTI = warTI.getId();
-	        Faction safeTI = FactionColl.get().getSafezone();
-	        String safeIdTI = safeTI.getId();
-	        
-	        //Etc other stuff
-	        Faction factionout;
-	        factionout = BoardColl.get().getFactionAt(PS.valueOf(eventti.getFrom()));
-	        if (mplayer.getFaction().getRelationTo(factionout) == Rel.TRUCE) {
-	        	return;
-	        } else {
-	            if (faction.getId() == warIdTI || faction.getId() == wildIdTI || faction.getId() == safeIdTI) {
-	                return;
-	            } else {
-	        	if (this.plugin.config.getBoolean("truceDenyTPINTO")) {
-	        // Checks if the player is neutral to the faction claims they are in
-	        if (mplayer.getFaction().getRelationTo(faction) == Rel.TRUCE) {
-	                eventti.setCancelled(true);
-	                player.sendMessage("§c§l[MCE] §r§cYou cannot tp into truce land.");
-	            }
-	        } else {
-	        	return;
-	        }
-	        	}
-	        }
-	  }
+    //Enemy
+    @EventHandler
+    private void onPlayerTPIn(PlayerTeleportEvent eventin) {
+
+        //Variables 		
+        // Get the player
+        Player player = eventin.getPlayer();
+        // Get the faction player
+        MPlayer mplayer = MPlayer.get(player);
+
+        // Setting up the faction variable for use later
+        Faction factionto;
+        // Get the faction for location TPing into
+        factionto = BoardColl.get().getFactionAt(PS.valueOf(eventin.getTo()));
+        boolean eTo = mplayer.getFaction().getRelationTo(factionto) == Rel.ENEMY;
+        boolean nTo = mplayer.getFaction().getRelationTo(factionto) == Rel.NEUTRAL;
+        boolean tTo = mplayer.getFaction().getRelationTo(factionto) == Rel.TRUCE;
+
+        // This gets wild, safe, and war zones defined so we can use it later in the checks!
+        Faction wild = FactionColl.get().getNone();
+        String wildId = wild.getId();
+        Faction war = FactionColl.get().getWarzone();
+        String warId = war.getId();
+        Faction safe = FactionColl.get().getSafezone();
+        String safeId = safe.getId();
+        if (this.plugin.config.getBoolean("enemyDenyTPINTO") == true) {
+            if (eTo == true && (!(factionto.getId() == warId || factionto.getId() == wildId || factionto.getId() == safeId))) {
+                eventin.setCancelled(true);
+                player.sendMessage("§c§l[MCE] §r§cYou cannot tp in to enemy land.");
+                if (this.plugin.config.getBoolean("enemyDenyTPINTO") == false) {
+                    return;
+                }
+                return;
+            } else {
+                if (this.plugin.config.getBoolean("neutralDenyTPINTO") == true) {
+                    if (nTo == true && (!(factionto.getId() == warId || factionto.getId() == wildId || factionto.getId() == safeId))) {
+                        eventin.setCancelled(true);
+                        player.sendMessage("§c§l[MCE] §r§cYou cannot tp in to neutral land.");
+                        if (this.plugin.config.getBoolean("neutralDenyTPINTO") == false) {
+                            return;
+                        }
+                    } else {
+                        if (this.plugin.config.getBoolean("truceDenyTPINTO") == true) {
+                            if (tTo == true && (!(factionto.getId() == warId || factionto.getId() == wildId || factionto.getId() == safeId))) {
+                                eventin.setCancelled(true);
+                                player.sendMessage("§c§l[MCE] §r§cYou cannot tp in to truce land.");
+                                if (this.plugin.config.getBoolean("truceDenyTPINTO") == false) {
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/com/ShlobdonG/gFTPDeny/TPOut.java
+++ src/com/ShlobdonG/gFTPDeny/TPOut.java
@@ -5,11 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
-
-
-
-
-
 import com.ShlobdonG.gFTPDeny.gFTPDenyMain;
 import com.massivecraft.factions.Rel;
 import com.massivecraft.factions.entity.BoardColl;
@@ -19,117 +14,63 @@ import com.massivecraft.factions.entity.MPlayer;
 import com.massivecraft.massivecore.ps.PS;
 
 public class TPOut implements Listener {
-	
-	private gFTPDenyMain plugin;
-	  public TPOut(gFTPDenyMain inst)
-	  {
-	    this.plugin = inst;
-	  }
-	  /* Enemy */
-	    // This one is to stop players from TPing out of enemy territory.
-	  @EventHandler
-	    private void onPlayerEnemyTPOut(PlayerTeleportEvent eventeo) {
-	        Player player = eventeo.getPlayer();
-	        MPlayer mplayer = MPlayer.get(player);
-	        Faction factionout;
-	        factionout = BoardColl.get().getFactionAt(PS.valueOf(eventeo.getTo()));
-	        if (mplayer.getFaction().getRelationTo(factionout) == Rel.ENEMY) {
-	        	return; 
-	        } else {
-	        // Get the config option for this event
-	        if (this.plugin.config.getBoolean("enemyDenyTPOUTOF")) {
-	        // Check to see if the faction claim player is standing on is enemy, then cancel the TP out of
-	        if (mplayer.isInEnemyTerritory()) {
-	            eventeo.setCancelled(true);
-	            player.sendMessage("§c§l[MCE] §r§cYou cannot tp out of enemy land.");
-	        } else {
-	        	return;
-	        }
-	        }
-	        }
-	    }
-	  
-	  @EventHandler
-	   /* Neutral */
-	    // This is so that players cant tp out of Neutral territory
-	    private void onPlayerNeutralTPOut(PlayerTeleportEvent eventno) {
-	        // The Player
-	        Player playerno = eventno.getPlayer();
-	        MPlayer mplayerno = MPlayer.get(playerno);
-	        // Making 'faction' usable later in code and so it can be defined.
-	        Faction factionno; 
-	        // Get the current relation of location claim player is at
-	        factionno = BoardColl.get().getFactionAt(PS.valueOf(eventno.getFrom()));
-	        
-	        // This gets wild, safe, and war zones defined so we can use it later in the checks!
-	        Faction wildno = FactionColl.get().getNone();
-	        String wildIdno = wildno.getId();
-	        Faction warno = FactionColl.get().getWarzone();
-	        String warIdno = warno.getId();
-	        Faction safeno = FactionColl.get().getSafezone();
-	        String safeIdno = safeno.getId();
-	        
-	        // Etc other stuff
-	        Faction factionout;
-	        factionout = BoardColl.get().getFactionAt(PS.valueOf(eventno.getTo()));
-	        if (mplayerno.getFaction().getRelationTo(factionout) == Rel.NEUTRAL) {
-	        	return;
-	        } else {
-	            // Checks if the faction is wild, safe or war zones, if so it will continue their teleport! Else, it will stop their teleport!
-	            if (factionno.getId() == warIdno || factionno.getId() == wildIdno || factionno.getId() == safeIdno) {
-	                return;
-	            } else {
-		        	if (this.plugin.config.getBoolean("neutralDenyTPOUTOF")) {
-	        // Checks if the player is neutral to the faction claims they are in 
-	        if (mplayerno.getFaction().getRelationTo(factionno) == Rel.NEUTRAL) {
-	                eventno.setCancelled(true);
-	                playerno.sendMessage("§c§l[MCE] §r§cYou cannot tp out of neutral land.");
-            } 
-        } else {
-            	return; 
-            	}
-	        }
-	        }
-	  }
-	    
-	  @EventHandler
-		   /* Truce */
-	    // This is so that players cant tp out of Truce territory
-	    private void onPlayerTruceTPOut(PlayerTeleportEvent eventio) {
-	        // The Player
-	        Player player = eventio.getPlayer();
-	        MPlayer mplayer = MPlayer.get(player);
-	        // Making 'faction' usable later in code and so it can be defined.
-	        Faction faction; 
-	        // Get the current relation of location claim player is at
-	        faction = BoardColl.get().getFactionAt(PS.valueOf(eventio.getFrom()));
-	        // Making Wild, Safe, & War zones usable later in the code.
-	        Faction wildTO = FactionColl.get().getNone();
-	        String wildIdTO = wildTO.getId();
-	        Faction warTO = FactionColl.get().getWarzone();
-	        String warIdTO = warTO.getId();
-	        Faction safeTO = FactionColl.get().getSafezone();
-	        String safeIdTO = safeTO.getId();
-	        
-	        // Etc other stuff
-	        Faction factionout;
-	        factionout = BoardColl.get().getFactionAt(PS.valueOf(eventio.getTo()));
-	        if (mplayer.getFaction().getRelationTo(factionout) == Rel.TRUCE) {
-	        	return;
-	        } else {
-	            if (faction.getId() == warIdTO || faction.getId() == wildIdTO || faction.getId() == safeIdTO) {
-	                return;
-	            } else {
-	        	if (this.plugin.config.getBoolean("truceDenyTPOUTOF")) {
-	        // Checks if the player is truce to the faction claims they are in 
-	        if (mplayer.getFaction().getRelationTo(faction) == Rel.TRUCE) {
-	                eventio.setCancelled(true);
-	                player.sendMessage("§c§l[MCE] §r§cYou cannot tp out of truce land.");
-	            } 
-	        } else {
-	            	return; 
-	            	}
-	        	}
-	        }
-	  }
+
+    // Allowing code in the main class be called.
+    private gFTPDenyMain plugin;
+    public TPOut(gFTPDenyMain inst) {
+            this.plugin = inst;
+        }
+        // Enemy
+    @EventHandler
+    public void onPlayerTPOut(PlayerTeleportEvent eventout) {
+        // Variables
+        // Get the player
+        Player player = eventout.getPlayer();
+        // Get the faction player
+        MPlayer mplayer = MPlayer.get(player);
+
+        // Setting up the faction variable for later use
+        Faction factionout;
+        factionout = BoardColl.get().getFactionAt(PS.valueOf(eventout.getFrom()));
+        boolean eFrom = mplayer.getFaction().getRelationTo(factionout) == Rel.ENEMY;
+        boolean nFrom = mplayer.getFaction().getRelationTo(factionout) == Rel.NEUTRAL;
+        boolean tFrom = mplayer.getFaction().getRelationTo(factionout) == Rel.TRUCE;
+
+        // This gets wild, safe, and war zones defined so we can use it later in the checks!
+        Faction wild = FactionColl.get().getNone();
+        String wildId = wild.getId();
+        Faction war = FactionColl.get().getWarzone();
+        String warId = war.getId();
+        Faction safe = FactionColl.get().getSafezone();
+        String safeId = safe.getId();
+        if (this.plugin.config.getBoolean("enemyDenyTPOUTOF") == true) {
+            if (eFrom == true && (!(factionout.getId() == warId || factionout.getId() == wildId || factionout.getId() == safeId))) {
+                eventout.setCancelled(true);
+                player.sendMessage("§c§l[MCE] §r§cYou cannot tp out of enemy land.");
+                if (this.plugin.config.getBoolean("enemyDenyTPOUTOF") == false) {
+                    return;
+                }
+            } else {
+                if (this.plugin.config.getBoolean("neutralDenyTPOUTOF") == true) {
+                    if (nFrom == true && (!(factionout.getId() == warId || factionout.getId() == wildId || factionout.getId() == safeId))) {
+                        eventout.setCancelled(true);
+                        player.sendMessage("§c§l[MCE] §r§cYou cannot tp out of neutral land.");
+                        if (this.plugin.config.getBoolean("neutralDenyTPOUTOF") == false) {
+                            return;
+                        }
+                    } else {
+                        if (this.plugin.config.getBoolean("truceDenyTPOUTOF") == true) {
+                            if (tFrom == true && (!(factionout.getId() == warId || factionout.getId() == wildId || factionout.getId() == safeId))) {
+                                eventout.setCancelled(true);
+                                player.sendMessage("§c§l[MCE] §r§cYou cannot tp out of truce land.");
+                                if (this.plugin.config.getBoolean("truceDenyTPOUTOF") == false) {
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
